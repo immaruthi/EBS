7d6adaa65e11ae0bc961a8de4186a8441957ef79
im maruthi <immaruthi@outlook.com>
2018-06-20 14:33:25 +0530

    CommitTestProject

---
+++ OpenHouseAPI/Application.Unit.EBSTest/ProductServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Application.Unit.EBSTest
+{
+    [TestClass]
+    public class ProductServiceTest
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            int a = 10;
+            int b = 20;
+            int c = a + b;
+            Assert.AreEqual(c, 30, "Test case failed");
+        }
+    }
+}
---
+++ OpenHouseAPI/Application.Unit.EBSTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Application.Unit.EBSTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Application.Unit.EBSTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ca558bb5-3b61-4983-9ead-8c9050e6cdf5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
