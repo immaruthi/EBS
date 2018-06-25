a8533c01be36361ece3bfa5d2496c922ce6fe277
maruthi p <maruthi@iamgood.in>
2018-06-25 16:58:15 +0530

    Added Test Project

---
+++ OpenHouseAPI/Application.TestBase/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Application.TestBase")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SAPIENT")]
+[assembly: AssemblyProduct("Application.TestBase")]
+[assembly: AssemblyCopyright("Copyright © SAPIENT 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("627f6f5d-c35f-4cfd-8bc0-f31172667655")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ OpenHouseAPI/Application.TestBase/SampleTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Application.ProductService;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Application.TestBase
+{
+    [TestClass]
+    public class SampleTest
+    {
+        [TestMethod]
+        public void GetAllProducts()
+        {
+            var obj = ProductServices.GetAllProducts();
+            Assert.IsNotNull(obj.Count, "Count shouldn't be null");
+        }
+
+        [TestMethod]
+        public void GetAllProducts2()
+        {
+            var obj = ProductServices.GetAllProducts();
+            Assert.IsNotNull(obj.Count, "Count shouldn't be null");
+        }
+    }
+}
